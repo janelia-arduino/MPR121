800da76eb072d879112157994f3ee0c12800bb9a
Peter Polidoro <peterpolidoro@gmail.com>
2018-01-17 10:29:53 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+# MPR121
+
+Authors:
+
+    Peter Polidoro <polidorop@janelia.hhmi.org>
+
+License:
+
+    BSD
+
+Proximity capacitive touch sensor controller Arduino library.
+
+[Examples](./examples)
---
+++ src/MPR121.h
@@ -0,0 +1,19 @@
+// ----------------------------------------------------------------------------
+// MPR121.h
+//
+// Authors:
+// Peter Polidoro polidorop@janelia.hhmi.org
+// ----------------------------------------------------------------------------
+#ifndef MPR121_H
+#define MPR121_H
+#include <Arduino.h>
+#include <Wire.h>
+
+
+class MPR121
+{
+public:
+private:
+};
+
+#endif
